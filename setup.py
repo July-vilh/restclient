2d9da460de72b6336992d6ef8fd15fd6376a5202
July-vilh <july.vilh@gmail.com>
2023-12-08 23:34:33 +0200

    created package restclient

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ REQUIRES = [
 
 setup(
     name='restclient',
-    version='0.0.1.',
+    version='0.0.1',
     packages=['restclient'],
     url='https://github.com/July-vilh/restclient.git',
     license='MIT',
